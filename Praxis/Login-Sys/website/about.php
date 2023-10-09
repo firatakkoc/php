b633c6ea7a396848fb1f01ea28477683ef4c4063
firatakkoc <firat.akkoc@outlook.com>
2023-10-09 21:50:50 +0200

    Praxis

---
+++ Praxis/Login-Sys/website/about.php
@@ -0,0 +1,9 @@
+
+<?php include "includes/header.php"; ?>
+
+<main>
+    <h1>Hakkımızda</h1>
+    <p>Biz, basit web tasarımları yaparız.</p>
+</main>
+
+<?php include "includes/footer.php"; ?>
---
+++ Praxis/Login-Sys/website/includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+        <p>&copy; 2023 Basit Web Sitesi</p>
+    </footer>
+    <script src="/assets/js/script.js"></script>
+</body>
+</html>
